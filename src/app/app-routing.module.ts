968dd24d7fdce3fe6a1435b6f08c6b8767383917
Aron Teh <aronteh.chess@gmail.com>
2021-11-11 11:06:14 +0000

    Added first Angular route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [];
+// Dedicated module for routing
+
+// Define routes here and the components they should load
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
+  // Router is configured at the app root level, configured with the routes
   imports: [RouterModule.forRoot(routes)],
+  // Exports it so the routes can be available throughout the app
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
